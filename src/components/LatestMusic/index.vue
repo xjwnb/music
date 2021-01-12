660ffa03a007d7d277082731729b88be2514085e
1565163445@qq.com <1565163445@qq.com>
2021-01-12 18:05:11 +0800

    update LatestMusic组件

--- src/components/LatestMusic/index.vue
+++ src/components/LatestMusic/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-12 17:28:06
- * @LastEditTime: 2021-01-12 17:31:11
+ * @LastEditTime: 2021-01-12 18:02:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \music\src\components\LatestMusic\index.vue
@@ -9,15 +9,28 @@
 <template>
   <div class="lastest-music">
     <div class="lastest-music-img">
-      <el-image></el-image>
+      <el-image :src="latestMusicData.picUrl"></el-image>
     </div>
     <div class="lastest-music-info">
-      
+      <div class="lastest-music-info-song-name">
+        {{ latestMusicData.name }}
+      </div>
+      <div class="lastest-music-info-song-artists">
+        {{ latestMusicData.artists.name }}
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LatestMusic",
+  props: {
+    latestMusicData: {}    // 最新音乐数据
+  }
+})
 </script>
 
 <style lang="less" scoped>
